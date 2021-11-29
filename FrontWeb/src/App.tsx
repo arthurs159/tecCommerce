cbb3557636fce283bdee9603746144cf99a71239
Arthur Scarpati <arthur_159@hotmail.com>
2021-11-29 14:53:20 -0300

    Project created

---
+++ FrontWeb/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return < NavBar/>
+}
+
+export default App;
+
---
+++ FrontWeb/src/components/NavBar/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+const NavBar = () => {
+       return(
+              <h1>TESTE NAVBAR</h1>
+       );
+};
+
+export default NavBar;
