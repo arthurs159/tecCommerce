81f798088c4c80930283ca842da46c4ca561c517
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-06 14:58:57 -0300

    Loader card

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -8,9 +8,11 @@ import { BASE_URL } from '../../util/requests';
 
 import './styles.css';
 import axios from 'axios';
+import ImageGrid from './CardLoader';
 
 const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Products>>();
+  const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
     const params: AxiosParams = {
@@ -22,9 +24,14 @@ const Catalog = () => {
       },
     };
 
-    axios(params).then((response) => {
-      setPage(response.data);
-    });
+    setisLoading(true);
+    axios(params)
+      .then((response) => {
+        setPage(response.data);
+      })
+      .finally(() => {
+        setisLoading(false);
+      });
   }, []);
 
   return (
@@ -37,15 +44,15 @@ const Catalog = () => {
 
       <div className="container">
         <div className="row">
-
-        {page?.content.map((product) => (
+          {( isLoading ? <h2>Buscando produtos... <ImageGrid /></h2>  : (
+            page?.content.map((product) => (
             <div className="col-sm-6" key={product.id}>
-            <NavLink to={`/catalog/ ${product.id}`}>
-              <Product product={product}/>
-            </NavLink>
+              <NavLink to={`/catalog/ ${product.id}`}>
+                <Product product={product} />
+              </NavLink>
             </div>
-        ))}
-          </div>
+          ))))}
+        </div>
       </div>
     </>
   );
